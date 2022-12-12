3d9eab46f2bf558b58b81e151610695d4f418752
huwso1 <120350205+huwso1@users.noreply.github.com>
2022-12-11 21:48:40 -0500

    Add files via upload

---
+++ Cine distrital/src/Modelo/FileManager/UsersData.java
@@ -0,0 +1,59 @@
+package Modelo.FileManager;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import Modelo.Usuarios.AbstractUser;
+public class UsersData implements AbstractData{
+	private Properties Prop;
+
+	public UsersData(){
+		 this.Prop=new Properties();
+	}
+	
+	public boolean Check(String code) throws IOException{
+		FileReader Rw=new FileReader("src/Users/RegisteredUsers.properties");
+		Prop.load(Rw);
+	    if(Prop.getProperty(code)!=null) {
+	    	
+	    	return true;
+	    }
+	    return false;
+	   
+	}
+	public String[] Getinfo(String code) throws IOException{
+		FileReader Rw=new FileReader("src/Users/RegisteredUsers.properties");
+		String[] info=new String[5];
+		Prop.load(Rw);
+		if(Prop.getProperty(code)!=null) {
+	    	return new String[0];
+	    }
+	   info[0]=code;
+	   info[1]=Prop.getProperty(code);
+	   for(int i=2;i<5;i++) {
+		   info[i]=Prop.getProperty(code+"."+i);
+	   }
+	   return info;
+	    	
+	   
+	}
+
+	@Override
+	public void RecordProfile(String[] user) throws IOException {
+		
+		FileReader Rw=new FileReader("src/Users/RegisteredUsers.properties");
+		String[] info;
+		Prop.load(Rw);
+		FileWriter Fw=new FileWriter("src/Users/RegisteredUsers.properties");
+		Prop.setProperty(user[0],user[1]);
+		for(int i=2;i<4;i++) {
+			   Prop.setProperty(user[0]+"."+i,user[i]);
+		   }
+		Prop.store(Fw,"");
+	}
+}
---
+++ Cine distrital/src/Modelo/Usuarios/AbstractUser.java
@@ -0,0 +1,16 @@
+package Modelo.Usuarios;
+
+public interface AbstractUser {
+    public String getid();
+	public String getname();
+	public String getrole();
+	public String getP();
+	public void setname(String name);
+	public void setrole(String role);
+	public void setid(String id);
+	public void setP(String P);
+	
+
+		
+	
+}
