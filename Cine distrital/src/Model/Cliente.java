625ccd345f5bba0b5dfa1e0d9002881b78d2f9ec
Holman Alvarado <alvholman@gmail.com>
2022-12-14 22:35:35 -0500

    actualizacion logica

--- Cine distrital/src/Model/Cliente.java
+++ Cine distrital/src/Model/Cliente.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.io.FileWriter;
+import java.io.FileReader;
+
 public class Cliente {
     private static Cliente instancia;
     private int idCliente;
@@ -8,6 +11,7 @@ public class Cliente {
     public Cliente(int idCliente){
         this.idCliente = idCliente;
         this.puntos = Puntos.getInstancia();
+        this.guardarCliente(idCliente);
     }
     
     public static Cliente getInstancia(int idCliente){
@@ -15,5 +19,18 @@ public class Cliente {
             instancia = new Cliente(idCliente);
         }
         return instancia;
+    }
+
+    public void guardarCliente(int numeroIdentificacion){
+
+        try {
+				FileWriter archivo = new FileWriter("archivo_identificacion.txt");
+                
+				archivo.write(Integer.toString(numeroIdentificacion));
+				archivo.close();
+				System.out.println("Su número de identificación se ha guardado correctamente en el archivo");
+			} catch (Exception e) {
+				System.out.println("Ha ocurrido un error al guardar el número de identificación");
+			}
     }
 }
--- Cine distrital/src/test/Main.java
+++ Cine distrital/src/test/Main.java
@@ -1,24 +1,47 @@
 package test;
 
 import java.io.FileNotFoundException;
+
+import Model.Cliente;
 import Model.FileManager.*;
 import Model.Usuarios.User;
 import Model.Usuarios.Worker;
 
+import java.util.Scanner;
+import java.io.FileWriter;
+
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FM a = new FM();
-		String[] Billinfo = new String[4];
-		Billinfo[0] = "22345";
-		Billinfo[1] = "Billy";
-		Billinfo[2] = "Seller";
-		Billinfo[3] = "992";
+	// public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/*
+	 * FM a = new FM();
+	 * String[] Billinfo = new String[4];
+	 * Billinfo[0] = "22345";
+	 * Billinfo[1] = "Billy";
+	 * Billinfo[2] = "Seller";
+	 * Billinfo[3] = "992";
+	 * 
+	 * Worker Billy = new Worker(Billinfo);
+	 * 
+	 * a.RecordProfile(Billy);
+	 */
 
-		Worker Billy = new Worker(Billinfo);
+	 public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese su número de identificación: ");
+		int numeroIdentificacion = sc.nextInt();
+		Cliente cliente = new Cliente(numeroIdentificacion);
 
-		a.RecordProfile(Billy);
-
+		/*
+		 * TODOS
+		 * 1. leer la cedula del cliente y guardarla en un archivo, si ya existe pues sigue
+		 * 2. seleccionar el multiplex, si ya existe sigue, sino lo crea
+		 * 3. seleccionar la película a ver
+		 * 4. seleccionar las sillas, al presionar crea la silla, si ya existe debe estar bloqueada
+		 * 5. seleccionar la cantidad de snacks
+		 * 6. mostrar la factura con la silla, los snakcs y los puntos
+		 */
 	}
+	// }
 
 }
